62c3f0f88dfb3a6e825aed2b33566a0febe6639b
dosepehr <dosepehr@gmail.com>
2023-06-29 15:32:06 +0330

    make cube

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ import * as THREE from 'three';
 // scene
 
 const scene = new THREE.Scene();
+
+// cube
+const geometry = new THREE.BoxGeometry(1, 1, 1); //width,height,depth
+const material = new THREE.MeshBasicMaterial({ color: '#ff00ff' });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube)
