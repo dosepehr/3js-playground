dd4c5017b8a54bf1fe53ecb613a060544204381a
dosepehr <dosepehr@gmail.com>
2023-06-29 16:30:29 +0330

    session 2 : 3js position

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const geometry = new THREE.BoxGeometry(1, 1, 1); //width,height,depth
 const material = new THREE.MeshBasicMaterial({ color: '#ff00ff' });
 const cube = new THREE.Mesh(geometry, material);
 
+cube.position.x = 0.7;
+cube.position.y = -0.6;
+cube.position.z = 1;
+
+// distance between cube center and scene center
+console.log(cube.position.length());
+console.log(cube.position.distanceTo(new THREE.Vector3(1, 1, 1)));
+// console.log(cube.position.distanceTo(camera.position));
+
+cube.position.normalize(); // makes cube length 1
+cube.position.set(1, -1, 1); // do all three vectors
 scene.add(cube);
 
 // camera
