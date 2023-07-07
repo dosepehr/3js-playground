7e88c61ff5269ba83e3f8196e0843bb11f749f83
dosepehr <dosepehr@gmail.com>
2023-07-08 03:10:16 +0330

    solve particle edges problem

--- particles.js
+++ particles.js
@@ -10,16 +10,35 @@ const canvas = document.querySelector('canvas');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, sizes.w / sizes.h);
 camera.position.z = 5;
+const textureLoader = new THREE.TextureLoader();
 
+const particleTexture = textureLoader.load('/textures/particles/2.png');
 /**
  * particles
  */
 // geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 5000;
+
+const positions = new Float32Array(count * 3);
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 100;
+}
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+);
 // material
 const particleMaterial = new THREE.PointsMaterial({
-    size: 0.02,
-    sizeAttenuation: true,
+    size: 30,
+    sizeAttenuation: false,
+    color: '#ff88cc',
+    transparent: true,
+    alphaMap: particleTexture,
+    // alphaTest: 0.001
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
 });
 // points
 const particles = new THREE.Points(particlesGeometry, particleMaterial);
