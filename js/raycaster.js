c9475f3d5881e5ecc77c3f7b255d47a7408f5e7b
dosepehr <dosepehr@gmail.com>
2023-07-12 06:22:32 +0330

    initial physics commit

--- js/raycaster.js
+++ js/raycaster.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
-import { handleFullscreen, handleResize } from './helpers/resize';
+import { handleFullscreen, handleResize } from '../helpers/resize';
 
 /**
  * Base
