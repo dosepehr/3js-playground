d4166359dcb2d750d5e41ea01bfa781d0489074c
dosepehr <dosepehr@gmail.com>
2023-07-12 06:41:35 +0330

    update sphere based on physics body

--- js/physics.js
+++ js/physics.js
@@ -149,13 +149,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
-
+let oldElapsedTime = 0;
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - oldElapsedTime;
+    oldElapsedTime = elapsedTime;
 
     // Update controls
     controls.update();
+    // update physics world
+    world.step(1 / 60, deltaTime, 3);
 
+    sphere.position.copy(sphereBody.position);
+    
     // Render
     renderer.render(scene, camera);
 
